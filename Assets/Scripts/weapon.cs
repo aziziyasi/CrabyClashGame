b402cf15b9690e82bdf28d87b05c013c839ccd34
Yasamin <aziziyasi>
2024-01-08 00:27:11 +0330

    add an animation for weapon

--- Assets/Scripts/weapon.cs
+++ Assets/Scripts/weapon.cs
@@ -13,13 +13,29 @@ public class Weapon : MonoBehaviour
     public Camera camera;
 
     private float nextFire;
+    
+    [Header("Ammo")]
+    public int mag = 5;
+    public int ammo = 30;
+    public int magAmmo = 30;
 
     [Header("VFX")]
     public GameObject hitVFX;
 
+    [Header("UI")]
+    public TextMeshProUGUI magText;
+    public TextMeshProUGUI ammoText;
+
+    [Header("Animation")]
+    public Animation animation;
+    public AnimationClip reload;
+
     void Start()
     {
        audio = GetComponent<AudioSource>();
+
+        magText.text = mag.ToString();
+        ammoText.text = ammo + "/" + magAmmo;
     }
     // Update is called once per frame
     void Update()
@@ -28,11 +44,35 @@ public class Weapon : MonoBehaviour
             nextFire -= Time.deltaTime;
         }
 
-        if (Input.GetButton("Fire1") && nextFire <=0 ) {
+        if (Input.GetButton("Fire1") && nextFire  <=0 && ammo > 0 && animation.isPlaying == false) {
             nextFire = 1 / fireRate;
+
+            ammo--;
+
+            magText.text = mag.ToString();
+            ammoText.text = ammo + "/" + magAmmo;
 
             Fire();
         }
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Reload();
+        }
+
+       
+    }
+
+    void Reload(){     
+
+        animation.Play(reload.name);
+        if (mag > 0) {
+            mag--;
+
+            ammo = magAmmo;
+        }
+
+        magText.text = mag.ToString();
+        ammoText.text = ammo + "/" + magAmmo;
     }
     void Fire() {
         Ray ray = new Ray(camera.transform.position,camera.transform.forward);
